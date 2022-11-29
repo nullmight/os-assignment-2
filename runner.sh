a9bbda6c8ea7f61dea497206fad124cc5bbacccb
Sai Panda <saiabcxyz9@gmail.com>
2022-11-29 19:56:36 +0530

    P1, P2 graphs for n=m=q=50

--- runner.sh
+++ runner.sh
@@ -5,19 +5,17 @@ gcc P2.c -o P2.out -lpthread
 gcc S.c -o S.out
 gcc transpose.c -o transpose.out
 
-array=(10)
+array=(10 50 100)
 for n in "${array[@]}"; do
-    for q in "${array[@]}"; do
-        for m in "${array[@]}"; do
-            python3 MatrixGen.py $n $q $m
-            wait $!
-            ./S.out $n $m $q inp1.txt inp2.txt out.txt
-            wait $!
-            python3 plot.py $n $q $m
-            wait $!
-            echo $n $q $m done
-        done
-    done
+    q=$n
+    m=$n
+    python3 MatrixGen.py $n $q $m
+    wait $!
+    ./S.out $n $m $q inp1.txt inp2.txt out.txt
+    wait $!
+    python3 plot.py $n $q $m
+    wait $!
+    echo $n $q $m done
 done
 # ./S.out 5 5 5 inp1.txt inp2.txt out.txt
 # ./P1.out 5 5 5 matrix1.txt matrix2.txt P1_1.csv 1 1
